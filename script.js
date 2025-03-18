0830309742fc94f1cacfee02083684f4769016ec
Android Studio <devandroid@ort>
2025-03-18 08:55:43 -0300

    Verificacion de notas

--- script.js
+++ script.js
@@ -25,9 +25,24 @@ function verifyEFSI(){
         nota.style.color = "red";
     }
 }
-function verifyForm()
+function verifyForm(func)
 {
     const matematica = document.getElementById("matematica").value;
     const lengua = document.getElementById("lengua").value;
     const efsi = document.getElementById("efsi").value;
+    if (matematica > 10 || matematica < 1 || lengua > 10 || lengua < 1 || efsi > 10 || efsi < 1) {
+        alert("Completa todo el formulario");
+    }
+    else{
+        switch (func) {
+            case 'averageNotes':
+                averageNotes();
+                break;
+            case 'highestScore':
+                highestScore();
+                break;
+            default:
+                break;
+        }
+    }
 }
