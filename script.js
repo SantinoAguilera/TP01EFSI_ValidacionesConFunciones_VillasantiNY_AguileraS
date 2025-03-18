99d4f608ad4d3112423b9d7d3da6a675660c174e
SANTINO AGUILERA <48313116@ort>
2025-03-18 08:23:32 -0300

    Added <script> to index.html

---
+++ script.js
@@ -0,0 +1,8 @@
+function verifyForm()
+{
+    const matematica = document.getElementById("matematica").value;
+    const lengua = document.getElementById("lengua").value;
+    const efsi = document.getElementById("efsi").value;
+
+    //if ()
+}
