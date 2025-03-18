7ce8345164a4d64de697d9da8de6f757ca1c7b88
SANTINO AGUILERA <48313116@ort>
2025-03-18 08:42:22 -0300

    Worked on script.js

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-function verifyForm()
+function averageNotes()
 {
     const matematica = document.getElementById("matematica").value;
     const lengua = document.getElementById("lengua").value;
     const efsi = document.getElementById("efsi").value;
+    let error = false;
 
-    //if ()
+    if (matematica.trim().length === 0)
+    {
+        error = true;
+        console.log("Error: matematica Has No Value")
+    }
+
+    if (lengua.trim().length === 0)
+    {
+        error = true;
+        console.log("Error: lengua Has No Value")
+    }
+
+    if (efsi.trim().length === 0)
+    {
+        error = true;
+        console.log("Error: efsi Has No Value")
+    }
+
+    if (!error)
+    {
+        let average = matematica + lengua + efsi;
+        document.getElementById("div").innerHTML = "El promedio de notas es: " + average;
+    }
 }
